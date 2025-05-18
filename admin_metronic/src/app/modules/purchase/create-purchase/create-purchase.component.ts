10c29ff2cd6e96749beb824a6949e9d9a895f34f
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-05-17 19:32:05 -0600

    Maquetado general del formulario de registro de compras

--- admin_metronic/src/app/modules/purchase/create-purchase/create-purchase.component.ts
+++ admin_metronic/src/app/modules/purchase/create-purchase/create-purchase.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-purchase.component.scss']
 })
 export class CreatePurchaseComponent {
+  full_name_user:string = '';
+  sucursal_user:string = '';
+  warehouse_id:string = '';
+  provider_id:string = '';
 
+  date_emision:string = '';
+  type_comprobant:string = '';
+  n_comprobant:string = '';
+  description:string = '';
+
+  // Detallado de la compra
+  search_products:string = '';
+  unit_id:string = '';
+  price_unit:number = 0;
+  quantity:number = 0;
+
+  importe:number = 0;
+  igv:number = 0;
+  total:number = 0;
+
+  listProducts(){}
 }
