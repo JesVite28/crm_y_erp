1f99f678fe8abb19c6111282d48c5abc03d8e38e
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-05-19 13:34:36 -0600

    Formulario de registro de transporte

---
+++ admin_metronic/src/app/modules/transports/edit-transports/edit-transports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-transports',
+  templateUrl: './edit-transports.component.html',
+  styleUrls: ['./edit-transports.component.scss']
+})
+export class EditTransportsComponent {
+
+}
---
+++ admin_metronic/src/app/modules/transports/list-transports/list-transports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-transports',
+  templateUrl: './list-transports.component.html',
+  styleUrls: ['./list-transports.component.scss']
+})
+export class ListTransportsComponent {
+
+}
