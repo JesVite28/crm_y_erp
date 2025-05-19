5fc8f579735789ed4e1b387b24d320b7ab55354d
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-05-19 14:08:35 -0600

    Listado de transportes, y formularios de edicion y eliminacion

--- admin_metronic/src/app/modules/transports/delete-transports/delete-transports.component.ts
+++ admin_metronic/src/app/modules/transports/delete-transports/delete-transports.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TransportsService } from '../service/transports.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-delete-transports',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-transports.component.scss']
 })
 export class DeleteTransportsComponent {
+
+  @Output() DeleteTransport: EventEmitter<any> = new EventEmitter();
+  @Input()  transport:any;
+
+  isLoading:any;
+  constructor(
+    public modal: NgbActiveModal,
+    public toast: ToastrService,
+    public transportService: TransportsService,
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  delete(){
+    this.transportService.deleteTransport(this.transport.id).subscribe((resp:any) => {
+      console.log(resp);
+      this.DeleteTransport.emit(resp);
+      this.modal.close();
+      this.toast.success("Exito","La eliminación se hizo correctamente");
+    })
+  }
 
 }
