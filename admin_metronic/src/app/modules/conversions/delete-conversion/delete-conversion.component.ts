58b1207258d002617ee741d80f2da81223956b05
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-05-20 10:18:21 -0600

    Finalizacion del modulo de Conversion

--- admin_metronic/src/app/modules/conversions/delete-conversion/delete-conversion.component.ts
+++ admin_metronic/src/app/modules/conversions/delete-conversion/delete-conversion.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ConversionService } from '../service/conversion.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-delete-conversion',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-conversion.component.scss']
 })
 export class DeleteConversionComponent {
+
+  @Output() DeleteConversion: EventEmitter<any> = new EventEmitter();
+  @Input()  conversion:any;
+
+  isLoading:any;
+  constructor(
+    public modal: NgbActiveModal,
+    public toast: ToastrService,
+    public conversionService: ConversionService,
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  delete(){
+    this.conversionService.deleteConversion(this.conversion.id).subscribe((resp:any) => {
+      console.log(resp);
+      if(resp.message == 403){
+        this.toast.error("Exito",resp.message_text);
+      }else{
+        this.DeleteConversion.emit(resp);
+        this.modal.close();
+        this.toast.success("Exito","La eliminación se hizo correctamente");
+      }
+    })
+  }
 
 }
--- api_crm_erp/app/Http/Resources/Product/Conversion/ConversionCollection.php
+++ api_crm_erp/app/Http/Resources/Product/Conversion/ConversionCollection.php
@@ -14,6 +14,8 @@ class ConversionCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "data" => ConversionResource::collection($this->collection)
+        ];
     }
 }
