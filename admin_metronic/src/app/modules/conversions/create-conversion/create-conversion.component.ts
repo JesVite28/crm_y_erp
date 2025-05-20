12f1575cf157c789c2de73603d9b4977aec29aab
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-05-19 21:36:41 -0600

    Listado de conversiones

---
+++ admin_metronic/src/app/modules/conversions/create-conversion/create-conversion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-conversion',
+  templateUrl: './create-conversion.component.html',
+  styleUrls: ['./create-conversion.component.scss']
+})
+export class CreateConversionComponent {
+
+}
---
+++ admin_metronic/src/app/modules/conversions/delete-conversion/delete-conversion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-conversion',
+  templateUrl: './delete-conversion.component.html',
+  styleUrls: ['./delete-conversion.component.scss']
+})
+export class DeleteConversionComponent {
+
+}
