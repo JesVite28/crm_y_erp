59e753970508c12f94b6c3f015f896bf4994b68c
EnriqueVal09 <quiquechavezrm@gmail.com>
2025-05-20 12:21:36 -0600

    Listado de contratos

---
+++ admin_metronic/src/app/modules/despacho/detail-contracts-despacho/detail-contracts-despacho.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-contracts-despacho',
+  templateUrl: './detail-contracts-despacho.component.html',
+  styleUrls: ['./detail-contracts-despacho.component.scss']
+})
+export class DetailContractsDespachoComponent {
+
+}
